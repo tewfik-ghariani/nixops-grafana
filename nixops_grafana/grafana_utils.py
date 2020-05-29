5a125fdfc073f0dff9567e537fea6619d2b79670
Tewfik Ghariani <tewfik.ghariani@infor.com>
2020-05-29 11:21:56 +0100

    updating poetry.lock + ci black is green now

--- nixops_grafana/grafana_utils.py
+++ nixops_grafana/grafana_utils.py
@@ -8,9 +8,6 @@ from typing import (
 from grafana_api.grafana_face import GrafanaFace
 
 
-def connect(
-    api_token: str, host: str, protocol: Union["http", "https"] = "https"
-):
+def connect(api_token: str, host: str, protocol: Union["http", "https"] = "https"):
     grafana_api = GrafanaFace(auth=api_token, host=host, protocol=protocol)
     return grafana_api
-
